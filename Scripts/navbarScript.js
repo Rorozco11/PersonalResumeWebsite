695a2a1f2df2e6c44a16d1188a4d713b648335cb
Rorozco11 <96450960+Rorozco11@users.noreply.github.com>
2024-10-13 21:36:33 -0400

    Hamburger menu for phone view

---
+++ Scripts/navbarScript.js
@@ -0,0 +1,5 @@
+// JavaScript for toggling the navbar
+document.getElementById('hamburger').addEventListener('click', function() {
+    var navbar = document.getElementById('navbar');
+    navbar.classList.toggle('active');
+});
