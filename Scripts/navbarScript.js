a3721d9ae73a0159a62fb40659af2bd8cbd1ff4a
Rorozco11 <96450960+Rorozco11@users.noreply.github.com>
2024-10-23 09:54:56 -0400

    "Hamburger Menu Resolved"

--- Scripts/navbarScript.js
+++ Scripts/navbarScript.js
@@ -3,3 +3,10 @@ document.getElementById('hamburger').addEventListener('click', function() {
     var navbar = document.getElementById('navbar');
     navbar.classList.toggle('active');
 });
+
+document.querySelectorAll('.navbar li a').forEach(item => {
+    item.addEventListener('click', function() {
+        var navbar = document.getElementById('navbar');
+        navbar.classList.remove('active'); // Remove the active class to hide the menu
+    });
+});
